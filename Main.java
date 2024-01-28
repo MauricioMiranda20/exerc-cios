2b6fd93f23438c65e7026fd03f440498522fb187
Mauricio Miranda <64110966+MauricioMiranda20@users.noreply.github.com>
2024-01-28 16:29:20 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -12,13 +12,13 @@ public class Main {
         
         Scanner escrever = new Scanner(System.in);
         
-        //System.out.print("Qual é seu peso: ");
-        //a = escrever.nextFloat();
+        System.out.print("Qual é seu peso: ");
+        a = escrever.nextFloat();
         
-        //System.out.print("Qual é sua altura: ");
-        //b = escrever.nextFloat();
+        System.out.print("Qual é sua altura: ");
+        b = escrever.nextFloat();
         
-        Avaliacao avaliacao = new Avaliacao(75.34,1.83);
+        Avaliacao avaliacao = new Avaliacao(a,b);
         
         System.out.println(avaliacao.getPeso());
         System.out.println(avaliacao.getAltura());
